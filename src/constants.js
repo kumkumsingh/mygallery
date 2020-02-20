3d72c0abbc69a184dbaa360e88b61987a365d13a
kumkumsingh <kumkumsingh.r@gmail.com>
2020-02-20 12:20:03 +0100

    Update constants.js
    added key

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 //client_id is a developer account access_key and it is required to fetch images from unsplash API
-const client_id = "You-api-key"
+const client_id = "509db171d8bf090c681b10fd3266dcd6b54fa9c56d1b8a2a673a217553bdd069"
 
 //This is the basic url to get photos of count 5 and client_id is required to access them from unsplash API.
 // export const url ="https://api.unsplash.com/photos?client_id="+client_id
